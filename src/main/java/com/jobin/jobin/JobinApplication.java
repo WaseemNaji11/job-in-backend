ed128934f5168c83f90e42b068a35842521d0f9b
WaseemNaji11 <waseemnaji29@gmail.com>
2023-09-22 21:38:06 +0100

    first commit

---
+++ src/main/java/com/jobin/jobin/JobinApplication.java
@@ -0,0 +1,13 @@
+package com.jobin.jobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobin/jobin/JobinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobin.jobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
